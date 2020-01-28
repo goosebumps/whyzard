e65e5cc75cc47ba07640e111e18434e9a51431a1
tomgoos@gmail.com <tomgoos@gmail.com>
2020-01-28 18:00:50 +0100

    Use form for searchterm and shop

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, RadioField, SubmitField
+from wtforms.validators import DataRequired
+import shop
+
+class SearchForm(FlaskForm):
+    searchterm = StringField('Zoekterm', validators=[DataRequired()])
+    # shopname = StringField('Shop', validators=[DataRequired()])
+    shopname = RadioField('Shop', choices=[("all", "all")] + list(zip(shop.shop_list, shop.shop_list)))
+    # password = PasswordField('Password', validators=[DataRequired()])
+    # remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Zoek')
--- main.py
+++ main.py
@@ -1,30 +1,22 @@
 # importing modules
-from flask import Flask, render_template, escape
+from flask import Flask, render_template, escape, flash, redirect
 import shop
+import forms
 
 # declaring app name
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "datraadjenooitpannekoek"
 
-
-@app.route("/")
-def root():
-    return searchshoppage("d12", escape("ben nevis"))
-
-
-@app.route("/<search_term>")
-def searchpage(search_term):
-    return searchshoppage("all", search_term)
-
-
-@app.route("/<shopname>/<search_term>")
-def searchshoppage(shopname, search_term):
-    shop_result = shop.get_shop_results(shopname, search_term)
-
+@app.route("/", methods=['GET', 'POST'])
+def searchshoppage():
+    form = forms.SearchForm()
+    shop_result = shop.get_shop_results(form.shopname.data, form.searchterm.data)
     return render_template(
         "index.html",
         shop_list=shop.shop_list,
         shop_result=shop_result,
-        search_term=search_term,
+        search_term=form.searchterm.data,
+        form=form
     )
 
 
