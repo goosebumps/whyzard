09b798f13762f58d136c61ef8200c4bb683c1fe6
Tom Goossens <tom.goossens@prodrive-technologies.com>
2020-02-25 16:07:07 +0100

    Fix currency parser for numbers larger than 999 euro

--- currency.py
+++ currency.py
@@ -7,6 +7,7 @@ regex = r"(\d*)[\.\,]?(\d*)"
 p = re.compile(regex)
 
 test_strings = [
+    "€1.309,95",
     "€54,95",
     "€54.95",
     "  €50",
@@ -18,16 +19,19 @@ test_strings = [
     "54.95",
 ]
 
+
 def StringToCurrency(test_str):
     s = re.sub(r"\s\s+", " ", test_str)
-    s = s.replace(",", ".")
     s = s.replace("€", "")
     s = s.replace("$", "")
+    s = s.replace(",", ".")  # all comma's do dots
+    s = re.sub(r"\.(?![^.]+$)|[^0-9.]", "", s)  # remove all but last dot
     try:
         f = float(s)
         return f
     except ValueError:
         return float('nan')
+
 
 if __name__ == "__main__":
     for test_str in test_strings:
